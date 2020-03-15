e0c6c9d32a4cdc6002b9dbc5695dfdaef0cea3be
edkondr <60880285+edkondr@users.noreply.github.com>
2020-03-16 01:05:09 +0300

    feat:add files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+  if (!array || array.length == 0)
   return 0;
+  else{
+        return Math.min.apply(Math, array);
+  }
 }
 
 exports.max = function max (array) {
+  if (!array || array.length == 0)
   return 0;
+  else{
+    return Math.max.apply(Math, array);
+  }
 }
 
 exports.avg = function avg (array) {
+  if (!array || array.length == 0)
   return 0;
+  else{
+    let a;
+    let b;
+        return array.reduce((a, b) => (a + b)) / array.length;
+  }
+  
 }
